3e35681e385cff8d6a53db7f284771ace764f75f
danielsuhcompsci <danielsuh112@yahoo.com>
2023-11-08 00:39:20 -0600

    initial commit

---
+++ backend/src/test/java/com/example/skateboardtricks/SkateboardtricksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skateboardtricks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkateboardtricksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
