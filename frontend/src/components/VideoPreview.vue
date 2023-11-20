dda44c46aaffe7a034488b98c35167a0bceef52a
danielsuhcompsci <danielsuh112@yahoo.com>
2023-11-19 19:16:25 -0600

    ability to upload multiple files, java server receives incoming files

--- frontend/src/components/VideoPreview.vue
+++ frontend/src/components/VideoPreview.vue
@@ -2,21 +2,27 @@
 import { ref } from 'vue'
 
 const files = ref<FileList | null>()
-const videoPreview = ref<HTMLVideoElement>()
+const videoPreviews = ref<string[]>([])
 const fileInput = ref<HTMLInputElement>()
 
-function previewVideo(file: File) {
-  if (!videoPreview.value) return
+function previewVideos(fileList: FileList) {
+  // if (!videoPreviews.value) videoPreviews.value = []
 
-  videoPreview.value!.src = URL.createObjectURL(file as Blob)
+  for (const file of fileList) {
+    videoPreviews.value.push(URL.createObjectURL(file as Blob))
+  }
+
+  console.log(videoPreviews.value)
+
+  // videoPreview.value!.src = URL.createObjectURL(file as Blob)
 }
 
 function handleFileInput(event: Event) {
   const target = event.target as HTMLInputElement
   if (target && target.files) {
     files.value = target.files
+    previewVideos(files.value)
   }
-  previewVideo(files.value![0])
 }
 
 function handleUpload() {
@@ -29,24 +35,54 @@ function handleUpload() {
     console.log('uploading...', files.value.item(i)?.name)
   }
 
-  files.value = null
+  const formData = new FormData()
+  for (const file of files.value) {
+    formData.append('files', file)
+    // file.arrayBuffer().then((buf) => {
+    //   console.log('Buf', buf)
+    // })
+  }
 
-  //   console.log(fileInput.value?.files)
-  fileInput.value!.files = null
-  fileInput.value!.value = ''
-  //   console.log(fileInput.value?.files)
+  // formData.append('file', files.value[0])
+
+  // console.log('uploading', files.value[0].name)
+
+  fetch('http://localhost:8080/api/upload', {
+    method: 'POST',
+    body: formData
+  })
+    .then((response) => {
+      if (response.ok) {
+        console.log('Upload successful')
+        files.value = null
+
+        //   console.log(fileInput.value?.files)
+        fileInput.value!.files = null
+        fileInput.value!.value = ''
+        videoPreviews.value = []
+        //   console.log(fileInput.value?.files)
+      } else {
+        console.error('Upload failed')
+      }
+    })
+    .catch((error) => {
+      console.error('Network error occurred', error)
+    })
 }
 </script>
 
 <template>
   <div class="flex flex-col items-center gap-5">
     <video
-      class="rounded-md md:max-w-[60vw]"
-      ref="videoPreview"
+      class="rounded-md md:max-w-[600px] max-h-full"
+      v-for="videoPreview in videoPreviews"
+      v-bind:src="videoPreview"
+      v-bind:key="videoPreview"
       controls
       accept="video/*"
       :class="{ hidden: !files }"
     />
+
     <button
       type="button"
       @click="handleUpload"
